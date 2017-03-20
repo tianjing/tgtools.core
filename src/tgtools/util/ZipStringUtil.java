64187d35617975f24f54caf8fcbee70681a09cfb
田径 <tian___jing@hotmail.com>
2017-03-20 14:43:35 +0800

    更新base64 编码对象

--- src/tgtools/util/ZipStringUtil.java
+++ src/tgtools/util/ZipStringUtil.java
@@ -3,6 +3,7 @@ package tgtools.util;
 import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
+import java.io.UnsupportedEncodingException;
 import java.util.zip.GZIPInputStream;
 import java.util.zip.GZIPOutputStream;
 import java.util.zip.ZipEntry;
@@ -18,50 +19,75 @@ public class ZipStringUtil {
 		if (primStr == null || primStr.length() == 0) {
 			return primStr;
 		}
+		try {
+			return org.apache.commons.codec.binary.Base64.encodeBase64String(gzip(primStr.getBytes("UTF-8")));
+		} catch (UnsupportedEncodingException e) {
+			LogHelper.error("","gzip压缩出错；字符集转换出错："+e.getMessage(),"ZipStringUtil.gzip(String)",e);
+		}
+		return null;
+	}
+	public static byte[] gzip(byte[] p_Content) {
+		if (p_Content == null || p_Content.length == 0) {
+			return p_Content;
+		}
 
 		ByteArrayOutputStream out = new ByteArrayOutputStream();
-		
+
 		GZIPOutputStream gzip = null;
 		try {
 			gzip = new GZIPOutputStream(out);
-			gzip.write(primStr.getBytes("UTF-8"));
+			gzip.write(p_Content);
 		} catch (IOException e) {
-			e.printStackTrace();
+			LogHelper.error("","GZIP压缩写入出错；原因："+e.getMessage(),"ZipStringUtil.gzip(byte[])",e);
 		} finally {
 			if (gzip != null) {
 				try {
 					gzip.close();
 				} catch (IOException e) {
-					e.printStackTrace();
+					LogHelper.error("","GZIP关闭流出错；原因："+e.getMessage(),"ZipStringUtil.gzip(byte[])",e);
 				}
 			}
 		}
-
-		return new sun.misc.BASE64Encoder().encode(out.toByteArray());
+		return out.toByteArray();
 	}
-
-	/**
-	 *
-	 * <p>
-	 * Description:使用gzip进行解压缩
-	 * </p>
-	 * 
-	 * @param compressedStr
-	 * @return
-	 */
+		/**
+         *
+         * <p>
+         * Description:使用gzip进行解压缩
+         * </p>
+         *
+         * @param compressedStr
+         * @return
+         */
 	public static String gunzip(String compressedStr) {
 		if (compressedStr == null) {
 			return null;
 		}
-		compressedStr=StringUtil.replace(compressedStr," ","+");
+
+		byte[] compressed = null;
+
+		try {
+			compressed =org.apache.commons.codec.binary.Base64.decodeBase64(compressedStr);
+			return gunzip(compressed);
+		}
+		catch (Exception e)
+		{
+			LogHelper.error("","gunzip解码失败；原因："+e.getMessage(),"ZipStringUtil.gunzip(String)",e);
+		}
+		return null;
+	}
+	public static String gunzip(byte[] compressedStr) {
+		if (compressedStr == null) {
+			return null;
+		}
+
 		ByteArrayOutputStream out = new ByteArrayOutputStream();
 		ByteArrayInputStream in = null;
 		GZIPInputStream ginzip = null;
 		byte[] compressed = null;
 		String decompressed = null;
 		try {
-			compressed = new sun.misc.BASE64Decoder()
-					.decodeBuffer(compressedStr);
+			compressed =compressedStr;
 			in = new ByteArrayInputStream(compressed);
 			ginzip = new GZIPInputStream(in);
 
@@ -72,7 +98,7 @@ public class ZipStringUtil {
 			}
 			decompressed = out.toString("UTF-8");
 		} catch (IOException e) {
-			e.printStackTrace();
+			LogHelper.error("","gunzip解码失败；原因："+e.getMessage(),"ZipStringUtil.gunzip(byte[])",e);
 		} finally {
 			if (ginzip != null) {
 				try {
@@ -93,17 +119,15 @@ public class ZipStringUtil {
 				}
 			}
 		}
-
 		return decompressed;
 	}
-
-	/**
-	 * 使用zip进行压缩
-	 * 
-	 * @param str
-	 *            压缩前的文本
-	 * @return 返回压缩后的文本
-	 */
+		/**
+         * 使用zip进行压缩
+         *
+         * @param str
+         *            压缩前的文本
+         * @return 返回压缩后的文本
+         */
 	public static final String zip(String str) {
 		if (str == null)
 			return null;
