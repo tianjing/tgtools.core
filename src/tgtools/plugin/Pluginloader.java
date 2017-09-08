c4f2bdffb2b34ef9b562d42144cde470746c7f02
田径 <tian___jing@hotmail.com>
2017-09-08 09:31:16 +0800

    修复平台加载和卸载插件不执行接口方法的问题

--- src/tgtools/plugin/Pluginloader.java
+++ src/tgtools/plugin/Pluginloader.java
@@ -1,6 +1,7 @@
 package tgtools.plugin;
 
 import tgtools.plugin.util.JARLoader;
+import tgtools.util.LogHelper;
 
 public class Pluginloader {
 
@@ -41,17 +42,26 @@ public class Pluginloader {
 				Object obj= clazz.newInstance();
 				if(null!=obj&& obj instanceof IPlugin){
 				m_Plugin=(IPlugin)obj;
+				m_Plugin.load();
 				m_IsLoad=true;
 				return true;
 				}
 			}
 		} catch (Exception e) {
-			e.printStackTrace();
+			LogHelper.error("","插件加载出错；原因："+e.getMessage(),"Pluginloader",e);
 		}
 		return false;
 	}
 	public void unload()
 	{
+		if(null!=m_Plugin)
+		{
+			try {
+				m_Plugin.unload();
+			} catch (Exception e) {
+				LogHelper.error("","插件卸载出错；原因："+e.getMessage(),"Pluginloader",e);
+			}
+		}
 		m_JARLoader=null;
 		m_Plugin=null;
 		m_IsLoad=false;
